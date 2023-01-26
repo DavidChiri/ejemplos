6bd33f728c3179b2e2c64c977a2a06ca3b27f4d2
David Chirivella <dachga01@floridauniversitaria.es>
2023-01-26 09:25:44 +0100

    ej2 finalizado

--- ej1.sh
+++ ej1.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-echo "introduce tu nombre: "
+echo "introduce tu nombre: " 
 read nombre
-echo "introduce primer apellido: "
+echo "introduce primer apellido: " 
 read ap1
-echo "introduce segundo apellido: "
+echo "introduce segundo apellido: " 
 read ap2
+
+sleep 5
 
 echo $nombre $ap1 $ap2
 
